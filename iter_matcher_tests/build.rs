87334107a0ab769df44e1c32877357d9224a9512
Daniel Parks <dp+git@oxidized.org>
2023-01-24 07:07:51 -0800

    Test generated all_entity_decode matcher.

--- iter_matcher_tests/build.rs
+++ iter_matcher_tests/build.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{BufWriter, Write};
 use std::path::Path;
 
@@ -15,6 +15,20 @@ fn main() -> Result<(), Box<dyn Error>> {
         .add(b"&gt;", "b'>'")
         .add(b"&quot;", "b'\"'")
         .render(&mut out, "pub fn basic_entity_decode", "u8")?;
+    writeln!(out)?;
+
+    writeln!(out, "/// Decode all HTML entities.")?;
+    let input = fs::read("html-entities.json")?;
+    let input: serde_json::Map<String, serde_json::Value> =
+        serde_json::from_slice(&input)?;
+    iter_matcher::Node::from_iter(input.iter().map(|(name, info)| {
+        (
+            name.as_bytes(),
+            format!("{:?}", info["characters"].as_str().unwrap()),
+        )
+    }))
+    .render(&mut out, "pub fn all_entity_decode", "&'static str")?;
+    writeln!(out)?;
 
     Ok(())
 }
--- iter_matcher_tests/tests/basic_entity_decode.rs
+++ iter_matcher_tests/tests/basic_entity_decode.rs
@@ -1,6 +1,5 @@
 use assert2::check;
-
-include!(concat!(env!("OUT_DIR"), "/test-matchers.rs"));
+use iter_matcher_tests::*;
 
 macro_rules! test {
     ($name:ident, $input:expr, $result:expr, $remainder:expr) => {
@@ -20,3 +19,4 @@ test!(entity, b"&amp;", Some(b'&'), b"");
 test!(entity_chars, b"&amp;abc", Some(b'&'), b"abc");
 test!(entity_entity, b"&amp;&lt;", Some(b'&'), b"&lt;");
 test!(short_entity_chars, b"&lt;abc", Some(b'<'), b"abc");
+test!(invalid_entity, b"&invalid;", None, b"&invalid;");
