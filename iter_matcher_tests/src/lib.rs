50df7b029017a3d2132f3b9c8a0274295ca56603
Daniel Parks <dp+git@oxidized.org>
2023-02-05 20:30:15 -0800

    Add `iter_matcher::render_stub()`
    Sometimes it’s necessary to have a function that looks like a matcher
    but doesn’t actually do anything. For example, if your matcher is
    extremely long, it might be better to limit lints to a stub.

--- iter_matcher_tests/src/lib.rs
+++ iter_matcher_tests/src/lib.rs
@@ -1,24 +1,2 @@
-#[cfg(not(feature = "cargo-clippy"))]
+// Include generated code.
 include!(concat!(env!("OUT_DIR"), "/test-matchers.rs"));
-
-// Stubs to make clippy happy. It is extremely slow with the real functions. See
-// https://github.com/rust-lang/rust-clippy/issues/10220
-
-macro_rules! stub {
-    ($name:ident, $ret:ty) => {
-        #[cfg(feature = "cargo-clippy")]
-        pub fn $name<'a, I>(iter: &mut I) -> Option<$ret>
-        where
-            I: Iterator<Item = &'a u8> + Clone,
-        {
-            // Just in case this actually gets used
-            iter.next();
-            None
-        }
-    };
-}
-
-stub!(all_entity_decode, &'static str);
-stub!(basic_entity_decode, u8);
-stub!(match_nothing, u8);
-stub!(match_nothing_true, bool);
