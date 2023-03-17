227871aee9c6657cd02d697190bffd67c4fc3410
Daniel Parks <dp+git@oxidized.org>
2023-03-16 20:48:43 -0700

    Don’t use _all_ HTML entities in tests
    Building matchers for all HTML entities can be a bit much for limited
    hosts, e.g. micro VMs or limited Docker containers. This removes most of
    the HTML entities; it only keeps entites that are all lowercase and
    whose names start with one of `[aglqt]`.
    
    This also fixes #14 (tests fail to build in Docker), so it removes the
    existing work-around.
    
    This resets all benchmarks.

---
+++ matchgen_tests/benches/most_entity_decode_iai.rs
@@ -0,0 +1,33 @@
+use iai::black_box;
+use matchgen_tests::*;
+use paste::paste;
+
+macro_rules! iai_benchmarks {
+    ( $( ($name:ident, $input:expr), )+ ) => {
+        paste! {
+            $(
+                fn [<$name _iter>]() -> Option<&'static str> {
+                    let input = black_box($input);
+                    let mut iter = input.iter();
+                    black_box(most_entity_decode(&mut iter))
+                }
+
+                fn [<$name _slice>]() -> (Option<&'static str>, &'static [u8]) {
+                    black_box(most_entity_decode_slice(
+                        black_box($input.as_slice())
+                    ))
+                }
+            )+
+
+            iai::main!(
+                $([<$name _iter>], [<$name _slice>],)+
+            );
+        }
+    }
+}
+
+iai_benchmarks! {
+    (timesbar, b"&timesbar;"),
+    (long_invalid, b"&xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+    (long_none, b"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+}
---
+++ matchgen_tests/tests/most_entity_decode.rs
@@ -0,0 +1,37 @@
+use assert2::check;
+use matchgen_tests::*;
+use paste::paste;
+
+macro_rules! test {
+    ($name:ident, $input:expr, $result:expr, $remainder:expr) => {
+        paste! {
+            #[test]
+            fn [<$name _iter>]() {
+                let input = $input;
+                let mut iter = input.iter();
+                check!(most_entity_decode(&mut iter) == $result);
+                check!(iter.as_slice() == $remainder);
+            }
+
+            #[test]
+            fn [<$name _slice>]() {
+                check!(
+                    most_entity_decode_slice($input)
+                    == ($result, $remainder.as_slice())
+                );
+            }
+        }
+    };
+}
+
+test!(nothing, b"", None, b"");
+test!(chars, b"abc", None, b"abc");
+test!(entity, b"&amp;", Some("&"), b"");
+test!(entity_chars, b"&amp;abc", Some("&"), b"abc");
+test!(entity_entity, b"&amp;&lt;", Some("&"), b"&lt;");
+test!(short_entity_chars, b"&lt;abc", Some("<"), b"abc");
+
+test!(timesbar, b"&timesbar;", Some("⨱"), b"");
+test!(timesb, b"&timesb;", Some("⊠"), b"");
+test!(times, b"&times;", Some("×"), b"");
+test!(times_bare_b, b"&timesb", Some("×"), b"b");
